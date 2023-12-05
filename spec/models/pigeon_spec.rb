b0c967ba80152a086d08c99357160c7215a14a8c
LEARN Academy <MichaelDReyes5@gmail.com>
2023-12-05 11:37:08 -0800

    made updated changes

--- spec/models/pigeon_spec.rb
+++ spec/models/pigeon_spec.rb
@@ -26,11 +26,20 @@ RSpec.describe Pigeon, type: :model do
     pigeon = Pigeon.create(
       name: 'Fly-Boy Freddy',
       age: 10,
-      # enjoys: 'long flights on the beach',
       image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3c11a4e-961b-47ec-8f72-acebce4c4562/dfxx0fp-cc0bc4f6-3117-4182-aeef-5f0ab72b7ead.jpg/v1/fill/w_1280,h_1920,q_75,strp/pigeon_wearing_fashion_clothes__on_podium_by_coolarts223_dfxx0fp-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzYzExYTRlLTk2MWItNDdlYy04ZjcyLWFjZWJjZTRjNDU2MlwvZGZ4eDBmcC1jYzBiYzRmNi0zMTE3LTQxODItYWVlZi01ZjBhYjcyYjdlYWQuanBnIiwiaGVpZ2h0IjoiPD0xOTIwIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvYzNjMTFhNGUtOTYxYi00N2VjLThmNzItYWNlYmNlNGM0NTYyXC9jb29sYXJ0czIyMy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.kr6DYBEpcBS2v4EdJoZ8VoG6JpF-OEWZJ4qwm1Bau_M'
     )
-  
+
     expect(pigeon.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should have an image" do
+    pigeon = Pigeon.create(
+      name: 'Fly-Boy Freddy',
+      age: 10,
+      enjoys: 'long flights on the beach' 
+    )
+       
+    expect(cat.errors[:image]).to_not be_empty
   end
 end
 
--- spec/requests/pigeons_spec.rb
+++ spec/requests/pigeons_spec.rb
@@ -105,7 +105,6 @@ RSpec.describe "Pigeons", type: :request do
     it "doesn't create a pigeon without a name" do 
       pigeon_params = {
         pigeon: {
-          # name: 'Pigeonardo DiCaprio',
           age: 13,
           enjoys: 'The company of younger pigeons between 9 and 10',
           image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3c11a4e-961b-47ec-8f72-acebce4c4562/dfubowo-3c759856-5ef4-43b1-a59c-53bc56250323.jpg/v1/fill/w_1280,h_1920,q_75,strp/very_stylish_pigeon_by_coolarts223_dfubowo-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzYzExYTRlLTk2MWItNDdlYy04ZjcyLWFjZWJjZTRjNDU2MlwvZGZ1Ym93by0zYzc1OTg1Ni01ZWY0LTQzYjEtYTU5Yy01M2JjNTYyNTAzMjMuanBnIiwiaGVpZ2h0IjoiPD0xOTIwIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvYzNjMTFhNGUtOTYxYi00N2VjLThmNzItYWNlYmNlNGM0NTYyXC9jb29sYXJ0czIyMy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.vX9suty-nFFCe6B8Sptmfc6ejREjjCZw6TCjobPOJ6Y'
@@ -124,7 +123,6 @@ RSpec.describe "Pigeons", type: :request do
       pigeon_params = {
         pigeon: {
           name: 'Pigeonardo DiCaprio',
-          age: 13,
           enjoys: 'The company of younger pigeons between 9 and 10',
           image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3c11a4e-961b-47ec-8f72-acebce4c4562/dfubowo-3c759856-5ef4-43b1-a59c-53bc56250323.jpg/v1/fill/w_1280,h_1920,q_75,strp/very_stylish_pigeon_by_coolarts223_dfubowo-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzYzExYTRlLTk2MWItNDdlYy04ZjcyLWFjZWJjZTRjNDU2MlwvZGZ1Ym93by0zYzc1OTg1Ni01ZWY0LTQzYjEtYTU5Yy01M2JjNTYyNTAzMjMuanBnIiwiaGVpZ2h0IjoiPD0xOTIwIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvYzNjMTFhNGUtOTYxYi00N2VjLThmNzItYWNlYmNlNGM0NTYyXC9jb29sYXJ0czIyMy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.vX9suty-nFFCe6B8Sptmfc6ejREjjCZw6TCjobPOJ6Y'
         }
@@ -169,6 +167,7 @@ RSpec.describe "Pigeons", type: :request do
     it "doesn't update a pigeon without a name" do 
       pigeon_params = {
         pigeon: {
+          name: 'Pigeonardo DiCaprio',
           age: 13,
           enjoys: 'The company of younger pigeons between 9 and 10',
           image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3c11a4e-961b-47ec-8f72-acebce4c4562/dfubowo-3c759856-5ef4-43b1-a59c-53bc56250323.jpg/v1/fill/w_1280,h_1920,q_75,strp/very_stylish_pigeon_by_coolarts223_dfubowo-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzYzExYTRlLTk2MWItNDdlYy04ZjcyLWFjZWJjZTRjNDU2MlwvZGZ1Ym93by0zYzc1OTg1Ni01ZWY0LTQzYjEtYTU5Yy01M2JjNTYyNTAzMjMuanBnIiwiaGVpZ2h0IjoiPD0xOTIwIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvYzNjMTFhNGUtOTYxYi00N2VjLThmNzItYWNlYmNlNGM0NTYyXC9jb29sYXJ0czIyMy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.vX9suty-nFFCe6B8Sptmfc6ejREjjCZw6TCjobPOJ6Y'
