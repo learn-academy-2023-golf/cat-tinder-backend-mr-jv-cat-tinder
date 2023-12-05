3b913e19f35fdfb36e620aaa584e31538ed8e498
LEARN Academy <MichaelDReyes5@gmail.com>
2023-12-05 14:48:20 -0800

    patched pigeon update and fixed enjoys attribute

--- spec/requests/pigeons_spec.rb
+++ spec/requests/pigeons_spec.rb
@@ -208,14 +208,29 @@ RSpec.describe "Pigeons", type: :request do
     end
 
     it "doesn't update a pigeon without an image" do 
-    pigeon_params = {
-      pigeon: {
-        name: 'Pigeonardo DiCaprio',
+      pigeon_params = {
+        pigeon: {
+          name: 'Pigeonardo DiCaprio',
           age: 13,
-          enjoys: 'The company of younger pigeons between 9 and 10'
+          enjoys: 'The company of younger pigeons between 9 and 10',
+          image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3c11a4e-961b-47ec-8f72-acebce4c4562/dfubowo-3c759856-5ef4-43b1-a59c-53bc56250323.jpg/v1/fill/w_1280,h_1920,q_75,strp/very_stylish_pigeon_by_coolarts223_dfubowo-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzYzExYTRlLTk2MWItNDdlYy04ZjcyLWFjZWJjZTRjNDU2MlwvZGZ1Ym93by0zYzc1OTg1Ni01ZWY0LTQzYjEtYTU5Yy01M2JjNTYyNTAzMjMuanBnIiwiaGVpZ2h0IjoiPD0xOTIwIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvYzNjMTFhNGUtOTYxYi00N2VjLThmNzItYWNlYmNlNGM0NTYyXC9jb29sYXJ0czIyMy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.vX9suty-nFFCe6B8Sptmfc6ejREjjCZw6TCjobPOJ6Y'
+        }
       }
-    }
+
       post '/pigeons', params: pigeon_params
+
+      pigeon = Pigeon.first
+
+      updated_pigeon_params = {
+        pigeon: {
+          name: 'Pigeonardo DiCaprio',
+          age: 13,
+          enjoys: 'The company of younger pigeons between 9 and 10. When not with his prefered aquaintances, he enjoys making pigeon rated movies',
+          image: ''
+        }
+      }
+
+      patch "/pigeons/#{pigeon.id}", params: updated_pigeon_params
       expect(response.status).to eq 422
       pigeon = JSON.parse(response.body)
       expect(pigeon['image']).to include "can't be blank"
